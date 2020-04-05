f22cc2389201a6aa6e814291e7c219bfbc97569f
Levi Pereira Leal Junior <levi.leal@resourcekraft.com>
2020-04-05 19:06:49 +0100

    ..

--- restaurant/core/views.py
+++ restaurant/core/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+import os
+
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'core/index.html')
+        context = {
+            'title': os.environ.get('APP_TITLE', '')
+        }
+        return render(request, 'core/index.html', context=context)
